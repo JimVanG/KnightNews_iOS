fca55af4822ffdea52aacfdaecda5f8cab53d3ff
JimVanG <jjvg@me.com>
2014-08-06 23:08:11 -0400

    Added story preview files

---
+++ KnightNews/JJVPreviewViewController.m
@@ -0,0 +1,38 @@
+//
+//  JJVPreviewViewController.m
+//  KnightNews
+//
+//  Created by james van gaasbeck on 8/6/14.
+//  Copyright (c) 2014 james van gaasbeck. All rights reserved.
+//
+
+#import "JJVPreviewViewController.h"
+
+@interface JJVPreviewViewController ()
+
+@end
+
+@implementation JJVPreviewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
