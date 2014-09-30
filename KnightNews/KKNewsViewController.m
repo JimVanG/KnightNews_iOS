4eb942035aded4994619ead38828081ed2b1ea28
kylelol <kyle.kirkland0528@me.com>
2014-09-30 00:12:42 -0400

    Removed the KKNewsVC.xib. Added in UITableView to the KKNewsVC. Updated App Delegate to instantiate the KKNewsVC

--- KnightNews/KKNewsViewController.m
+++ KnightNews/KKNewsViewController.m
@@ -8,8 +8,9 @@
 
 #import "KKNewsViewController.h"
 
-@interface KKNewsViewController ()
+@interface KKNewsViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property (nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation KKNewsViewController
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self setUpTableView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,6 +28,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Interface Setup Methods 
+-(void)setUpTableView
+{
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableViewDataSource Methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if ( !cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    return cell;
+    
+}
+
+#pragma mark - UITableViewDelegate Methods
 /*
 #pragma mark - Navigation
 
