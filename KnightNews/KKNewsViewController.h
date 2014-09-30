659572542a32eba5c93c7a3ebaafc211b32c7d96
kylelol <kyle.kirkland0528@me.com>
2014-09-29 23:37:40 -0400

    Initially added KKnewsVC to the project.

---
+++ KnightNews/KKNewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKNewsViewController.h
+//  KnightNews
+//
+//  Created by Kyle Kirkland on 9/29/14.
+//  Copyright (c) 2014 james van gaasbeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKNewsViewController : UIViewController
+
+@end
