502c85348d57e874edf38f8ef917eb90ede76345
kylelol <kyle.kirkland0528@me.com>
2014-09-30 00:16:58 -0400

    Added KKNewsTableViewCell. Registered with table view in KKNewsVC.

--- KnightNews/KKNewsViewController.m
+++ KnightNews/KKNewsViewController.m
@@ -33,6 +33,8 @@
 {
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
     
+    [self.tableView registerNib:[UINib nibWithNibName:@"KKNewsTableViewCell" bundle:nil]forCellReuseIdentifier:@"NewsCell"];
+    
     [self.view addSubview:self.tableView];
 }
 
