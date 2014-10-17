5e2acad122e3ddd6cafa4b796cb3082931ccc6f9
JimVanG <jjvg@me.com>
2014-10-17 14:47:26 -0400

    Added blank JJVSettingsTableViewController to house the settings VC.

---
+++ KnightNews/JJVSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJVSettingsTableViewController.h
+//  KnightNews
+//
+//  Created by james van gaasbeck on 10/17/14.
+//  Copyright (c) 2014 james van gaasbeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJVSettingsTableViewController : UITableViewController
+
+@end
