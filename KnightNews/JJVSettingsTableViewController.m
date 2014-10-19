5e7d9ec5d39481997583b8be2643cc8321178542
JimVanG <jjvg@me.com>
2014-10-19 00:51:22 -0400

    Opening up knightnews about url, changed repo url to open up in default browser, deleted aboutVCs.

--- KnightNews/JJVSettingsTableViewController.m
+++ KnightNews/JJVSettingsTableViewController.m
@@ -104,38 +104,38 @@
 }
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 #pragma mark - Table view delegate
 
@@ -150,19 +150,26 @@
     switch (indexPath.section) {
         case 0:{
             //Open up About VC
-
+            JJVWebViewController *webVC = [[JJVWebViewController alloc] init];
+            webVC.urlRequest = [NSURLRequest requestWithURL:
+                                [NSURL URLWithString:
+                                 @"http://knightnews.com/about/"]];
+            [self.navigationController pushViewController: webVC animated: YES];
             break;
         }
             
         case 1:{
             //Open up repo link in our webviewVC
+//            JJVWebViewController *webVC = [[JJVWebViewController alloc] init];
+//            webVC.urlRequest = [NSURLRequest requestWithURL:
+//                                [NSURL URLWithString:
+//                                 @"https://github.com/JimVanG/KnightNews_iOS"]];
+//            [self.navigationController pushViewController: webVC animated: YES];
             
-            JJVWebViewController *webVC = [[JJVWebViewController alloc] init];
-            webVC.urlRequest = [NSURLRequest requestWithURL:
-                                [NSURL URLWithString:
-                                 @"https://github.com/JimVanG/KnightNews_iOS"]];
-            [self.navigationController pushViewController: webVC animated: YES];
-            
+            //Open app in Safari
+            [[UIApplication sharedApplication] openURL:
+                                  [NSURL URLWithString:
+                                   @"https://github.com/JimVanG/KnightNews_iOS"]];
             break;
         }
         default:{
@@ -174,13 +181,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
